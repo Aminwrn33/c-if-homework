69b0ed77cc51b71ffe66852714cd8ba99d73725b
Aminwrn33 <aminmaharramli528@gmail.com>
2024-07-18 10:06:12 +0400

    Update c++ if homework.cpp

--- c++ if homework.cpp
+++ c++ if homework.cpp
@@ -1,1 +1,202 @@
-﻿
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /*cout << "1. tapsiriq\n\n";
+
+    int number;
+    cout << "Ededi daxil edin: ";
+    cin >> number;
+
+    if (number % 2 == 0)
+    {
+        cout << number << " cut ededdir" << endl;
+    }
+    else
+    {
+        cout << number << " tek ededdir" << endl;
+    }*/
+
+    /*return 0;*/
+
+
+
+    /*cout << "2. tapsiriq\n\n";
+
+    int num1, num2;
+    cout << "iki eded daxil edin: ";
+    cin >> num1 >> num2;
+
+    if (num1 < num2)
+    {
+        cout << "Kicik eded " << num1 << "dir" << endl;
+    }
+    else if (num2 < num1)
+    {
+        cout << "Kicik eded " << num2 << "dir" << endl;
+    }
+    else
+    {
+        cout << "Her iki eded beraberdir" << endl;
+    }
+
+    return 0;*/
+
+
+
+    /*cout << "tapsiriq 5\n\n";
+
+    int number;
+    cout << "eded daxil edin: ";
+    cin >> number;
+
+    if (number >= 1 && number <= 50)
+    {
+        cout << number << " 1-50 araligindadi" << endl;
+    }
+    else
+    {
+        cout << number << " 1-50 araliginda deyil" << endl;
+    }
+
+    return 0;*/
+
+
+
+    /* cout << "tapsiriq 6\n\n";
+
+     int X, Y;
+     cout << "iki eded daxil edin X,Y: ";
+     cin >> X >> Y;
+
+     if (Y != 0)
+     {
+         if (X % Y == 0)
+         {
+             cout << "Yes" << endl;
+         }
+         else
+         {
+             cout << "No" << endl;
+         }
+     }
+     else
+     {
+         cout << "Y sifira beraber ola bilmez" << endl;
+     }
+
+     return 0;*/
+
+
+
+     /*cout << "tapsiriq 9\n\n";
+
+     int number;
+     cout << "Eded daxil edin: ";
+     cin >> number;
+
+     if (number < 0)
+     {
+         number = -number;
+     }
+
+     cout << "ededin modulu: " << number << endl;
+
+     return 0;*/
+
+
+
+     /*cout << "tapsiriq 3\n\n";
+
+     int number;
+     cout << "eded daxil edin: ";
+     cin >> number;
+
+     if (number > 0)
+     {
+         cout << "eded musbetdir" << endl;
+     }
+     else if (number < 0)
+     {
+         cout << "eded menfidir" << endl;
+     }
+     else
+     {
+         cout << "eded sifirdir." << endl;
+     }
+
+     return 0;*/
+
+
+
+     /*cout << "tapsiriq 8\n\n";
+
+     int number;
+     cout << "eded daxil edin: ";
+     cin >> number;
+
+     if (number % 3 == 0)
+     {
+         cout << number << " 3-e qaliqsiz bolunur." << endl;
+     }
+     else
+     {
+         cout << number << " 3-e qaliqli bolunur." << endl;
+     }
+
+     if (number % 5 == 0)
+     {
+         cout << number << " 5-e qaliqsiz bolunur." << endl;
+     }
+     else
+     {
+         cout << number << " 5-e qaliqli bolunur." << endl;
+     }
+
+     if (number % 7 == 0)
+     {
+         cout << number << " 7-ye qaliqsiz bolunur." << endl;
+     }
+     else
+     {
+         cout << number << " 7-ye qaliqli bolunur." << endl;
+     }
+
+     return 0;*/
+
+
+
+     /*cout << "tapsiriq 10\n\n";
+
+     int number;
+     cout << "Maximum 4 reqemli eded daxil edin: ";
+     cin >> number;
+
+     if (number >= 0 && number <= 9999)
+     {
+         if (number < 10)
+         {
+             cout << "eded 1 reqemden ibaretdir" << endl;
+         }
+         else if (number < 100)
+         {
+             cout << "eded 2 reqemden ibaretdir" << endl;
+         }
+         else if (number < 1000) {
+
+             cout << "eded 3 reqemden ibaretdir" << endl;
+         }
+         else
+         {
+             cout << "eded 4 reqemden ibaretdir" << endl;
+         }
+     }
+     else
+     {
+         cout << "bu eded 4 reqemden coxdur" << endl;
+     }
+
+     return 0;*/
+
+}
